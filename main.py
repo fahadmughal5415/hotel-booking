0b018170a004678b7560165786ccb88de915d3a9
Programming Point <fahadmughal5415@gmail.com>
2025-02-02 08:25:20 +0500

    Enhance Hotel class with booking and availability methods; update main.py for user interaction and CSV handling

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv("hotels.csv", dtype={"id": str})
+
+
 class Hotel:
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
     def book(self):
-        pass
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
     def book(self):
         pass
@@ -19,11 +29,14 @@ class ReservationTicket:
         pass
 
 
-id = input("Enter the id of the hotel")
-hotel = Hotel(id)
+print(df)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
 
-name = input("Enter the name of the customer: ")
-reservation = ReservationTicket(name, hotel)
-reservation.generate()
+    name = input("Enter the name of the customer: ")
+    reservation = ReservationTicket(name, hotel)
+    print(reservation.generate())
+else:
+    print("Hotel is not available")
