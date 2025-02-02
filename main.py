994e3c8055e58bdf2312317060cea7796cf051a6
Programming Point <fahadmughal5415@gmail.com>
2025-02-02 10:44:59 +0500

    Add credit card validation and integrate with hotel booking process

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -32,16 +33,42 @@ class ReservationTicket:
     Hotel Name: {self.hotel.name}
 """
         return content
+    
+
+class CreditCard:
+    def __init__(self, card_number):
+        self.card_number = card_number
+    
+    def validate(self, expiration, holder, cvv):
+        # Implement your credit card validation logic here
+        self.expiration = expiration
+        self.holder = holder
+        self.cvv = cvv
+        card_data = {"number": self.card_number, "expiration": self.expiration, "holder": self.holder, "cvv": self.cvv}
+
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+        
 
 
 print(df)
+print(df_cards)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 if hotel.available():
-    hotel.book()
-
-    name = input("Enter the name of the customer: ")
-    reservation = ReservationTicket(name, hotel)
-    print(reservation.generate())
+    number = input("Enter the credit card number: ")
+    expiration = input("Enter the expiration date (MM/YY): ")
+    holder = input("Enter the credit card holder's name: ")
+    cvv = input("Enter the CVV: ")
+    credit_card = CreditCard(number)
+    if credit_card.validate(expiration, holder, cvv):
+        hotel.book()
+        name = input("Enter the name of the customer: ")
+        reservation = ReservationTicket(name, hotel)
+        print(reservation.generate())
+    else:
+        print("Invalid credit card details")
 else:
     print("Hotel is not available")
