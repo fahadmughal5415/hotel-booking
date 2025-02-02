9d6d462f52483423c6a9ac496f71b28cdbc3cd98
Programming Point <fahadmughal5415@gmail.com>
2025-02-02 07:44:05 +0500

    Implement Hotel and ReservationTicket classes with basic methods and user input for booking

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 class Hotel:
+    def __init__(self, id):
+        pass
+    def book(self):
+        pass
+
+    def available(self):
+        pass
+
     def book(self):
         pass
 
 
 class ReservationTicket:
+
+    def __init__(self, customer_name, hotel_object):
+        pass
     def generate(self):
         pass
+
+
+id = input("Enter the id of the hotel")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+
+name = input("Enter the name of the customer: ")
+reservation = ReservationTicket(name, hotel)
+reservation.generate()
